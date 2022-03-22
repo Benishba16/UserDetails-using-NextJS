6004ba50b1ea0edc48c13e3e418bcdd646138826
BenishbaM <benishba@thelaunchclub.in>
2022-03-22 06:28:30 +0530

    Added TailwindCSS

--- components/User.js
+++ components/User.js
@@ -2,16 +2,20 @@ import Link from "next/link"
 
 function User({ userData, detail }) {
     return <div>
-        <h1>User</h1>
+        <div className="flex text-left border-2 border-teal-500 rounded-xl">
+            <div className="p-3 w-1/2 font-bold  text-2xl">Name</div>
+            <div className="p-3 w-1/2 font-bold text-2xl">Email</div>
+        </div>
         {
             userData.filter((data) => data.name.toLowerCase().includes(detail) || data === "" )
             // userData
             .map((data) => (
-                <div key={data.id}>
-                    <Link href={`/users/${data.id}`}>
-                    <p>{data.name}</p>
-                    </Link>
+                <Link href={`/users/${data.id}`}>
+                <div className="cursor-pointer m-1 bg-white hover:bg-slate-500 hover:text-white flex text-left bg-white-500 rounded-xl p-2 text-xl" key={data.id}>
+                    <div className="w-1/2">{data.name}</div>
+                    <div className="w-1/2" >{data.email}</div>
                 </div>
+                </Link>
             ))
         }
     </div>
